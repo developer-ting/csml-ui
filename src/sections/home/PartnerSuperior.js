7f609cd95c84768bfd161c7825232db593bb90f9
VipVishal007 <mvishu405@gmail.com>
2023-11-06 21:59:40 +0530

    border animation

--- src/sections/home/PartnerSuperior.js
+++ src/sections/home/PartnerSuperior.js
@@ -12,140 +12,183 @@ import "slick-carousel/slick/slick-theme.css";
 // STYLES //
 import styles from "../../styles/sections/home/PartnerSuperior.module.scss";
 
-
 // IMAGES //
 import partner_img from "../../../public/img/home/partner_img.jpg";
 import partner_img_two from "../../../public/img/home/partner_img_two.jpg";
 import yellow_img from "../../../public/img/home/yellow_img.jpg";
 import arrow from "../../../public/img/arrow.svg";
 
-
 /** Home Hero Section */
 export default function PartnerSuperior() {
-  var settings = {
+	var settings = {
 		dots: true,
 		arrows: false,
 		infinite: true,
 		slidesToShow: 1,
-    slidesToScroll: 1,
+		slidesToScroll: 1,
 		autoplay: true,
 		pauseOnHover: false,
 		speed: 2000,
-    autoplaySpeed: 5000,
-		
+		autoplaySpeed: 5000,
 	};
 
 	return (
-    <section className={`${styles.partner_superior_wrap} ptb_100`}>
-      <div className="container">
-        <div className={`${styles.section_title} pb_40`}>
-          <h2 className="heading_text_55 color_white pb_20">Your partner in superior <br className="hidden-xs" />amusement experience </h2>
-          <p className="paraTxt color_white">Discover how CSML can help you achieve entertainment centre success, from concept to operation. Our expertise streamlines planning, design, logistics, staffing, pricing and support.</p>
-        </div>
-        <div className={`${styles.partner_superior_flex} f_j`}>
-          <div className={`${styles.partner_superior_box}`}>
-            <div className={`${styles.partner_superior_content}`}>
-              <img className="border_8" src={partner_img.src} alt="img" />
-              <div className={`${styles.desc_box}`}>
-                <div className={`${styles.desc_title} f_j`}>
-                  <h3 className="text_24 color_white text_500 pb_20">Consultancy Services</h3>
-                  <a href="#" rel="noreferrer">
-                    <button className="btn_arrow">
-                      <span className={`${styles.arrow_one} arrow_one`}><img src={arrow.src} /></span>
-                      <span className={`${styles.arrow_two} arrow_two`}><img src={arrow.src} /></span>
-                    </button>
-                  </a>
-                </div>
-                <p className="paraTxt color_white">Tailored consultancy services for Indoor Amusement Centers (IAC), simplifying operations, ensuring seamless coordination from start to finish.</p>
-              </div>
-            </div>
-          </div>
-          <div className={`${styles.partner_superior_box}`}>
-            <div className={`${styles.partner_superior_content}`}>
-              <img className="border_8" src={partner_img_two.src} alt="img" />
-              <div className={`${styles.desc_box}`}>
-                <div className={`${styles.desc_title} f_j`}>
-                  <h3 className="text_24 color_white text_500 pb_20">Management Contract - <br className="hidden-xs" />Operational Excellence</h3>
-                  <a href="#" rel="noreferrer">
-                    <button className="btn_arrow">
-                      <span className={`${styles.arrow_one} arrow_one`}><img src={arrow.src} /></span>
-                      <span className={`${styles.arrow_two} arrow_two`}><img src={arrow.src} /></span>
-                    </button>
-                  </a>
-                </div>
-                <p className="paraTxt color_white">Operational excellence is offered via FEC management contract. Our expert team manages day-to-day operations, allowing clients to focus on strategic goals.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.yellow_main} pt_100`}>
-          <Slider {...settings}> 
-            <div className={`${styles.yellow_item}`}>
-              <div className={`${styles.yellow_box} f_j`}>
-                <div className="dot_two dots_p"></div>
-                <div className="dot_three dots_p"></div>
-                <div className={`${styles.yellow_content}`}>
-                  <h3 className="text_35 pb_20">Elevate your home with custom entertainment solutions</h3>
-                  <p className="paraTxt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                  <div className={`${styles.yellow_btn} pt_30`}>
-                    <a href="#" rel="noreferrer">
-                      <button className="btn_project_default yellow_btn">
-                        Learn More
-                      </button>
-                    </a>
-                  </div>
-                </div>
-                <div className={`${styles.yellow_img}`}>
-                  <img className="" src={yellow_img.src} alt="img" />
-                </div>
-              </div>
-            </div>
-            
-            <div className={`${styles.yellow_item}`}>
-              <div className={`${styles.yellow_box} f_j`}>
-                <div className="dot_two dots_p"></div>
-                <div className="dot_three dots_p"></div>
-                <div className={`${styles.yellow_content}`}>
-                  <h3 className="text_35 pb_20">Elevate your home with custom entertainment solutions</h3>
-                  <p className="paraTxt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                  <div className={`${styles.yellow_btn} pt_30`}>
-                    <a href="#" rel="noreferrer">
-                      <button className="btn_project_default yellow_btn">
-                        Learn More
-                      </button>
-                    </a>
-                  </div>
-                </div>
-                <div className={`${styles.yellow_img}`}>
-                  <img className="" src={yellow_img.src} alt="img" />
-                </div>
-              </div>
-            </div>
-            <div className={`${styles.yellow_item}`}>
-              <div className={`${styles.yellow_box} f_j`}>
-                <div className="dot_two dots_p"></div>
-                <div className="dot_three dots_p"></div>
-                <div className={`${styles.yellow_content}`}>
-                  <h3 className="text_35 pb_20">Elevate your home with custom entertainment solutions</h3>
-                  <p className="paraTxt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                  <div className={`${styles.yellow_btn} pt_30`}>
-                    <a href="#" rel="noreferrer">
-                      <button className="btn_project_default yellow_btn">
-                        Learn More
-                      </button>
-                    </a>
-                  </div>
-                </div>
-                <div className={`${styles.yellow_img}`}>
-                  <img className="" src={yellow_img.src} alt="img" />
-                </div>
-              </div>
-            </div>
-          </Slider>
-          
-        </div>
-      </div>
+		<section className={`${styles.partner_superior_wrap} ptb_100`}>
+			<div className="container">
+				<div className={`${styles.section_title} pb_40`}>
+					<h2 className="heading_text_55 color_white pb_20">
+						Your partner in superior <br className="hidden-xs" />
+						amusement experience{" "}
+					</h2>
+					<p className="paraTxt color_white">
+						Discover how CSML can help you achieve entertainment centre success, from
+						concept to operation. Our expertise streamlines planning, design,
+						logistics, staffing, pricing and support.
+					</p>
+				</div>
 
-    </section>
-  );
+				<div className={`${styles.partner_superior_flex} f_j`}>
+					<div className={`${styles.partner_superior_box} border_animation`}>
+						<div className={`${styles.partner_superior_content}`}>
+							<img className="border_8" src={partner_img.src} alt="img" />
+							<div className={`${styles.desc_box}`}>
+								<div className={`${styles.desc_title} f_j`}>
+									<h3 className="text_24 color_white text_500 pb_20">
+										Consultancy Services
+									</h3>
+									<a href="#" rel="noreferrer">
+										<button className="btn_arrow">
+											<span className={`${styles.arrow_one} arrow_one`}>
+												<img src={arrow.src} />
+											</span>
+											<span className={`${styles.arrow_two} arrow_two`}>
+												<img src={arrow.src} />
+											</span>
+										</button>
+									</a>
+								</div>
+								<p className="paraTxt color_white">
+									Tailored consultancy services for Indoor Amusement Centers (IAC),
+									simplifying operations, ensuring seamless coordination from start to
+									finish.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className={`${styles.partner_superior_box} border_animation`}>
+						<div className={`${styles.partner_superior_content}`}>
+							<img className="border_8" src={partner_img_two.src} alt="img" />
+							<div className={`${styles.desc_box}`}>
+								<div className={`${styles.desc_title} f_j`}>
+									<h3 className="text_24 color_white text_500 pb_20">
+										Management Contract - <br className="hidden-xs" />
+										Operational Excellence
+									</h3>
+									<a href="#" rel="noreferrer">
+										<button className="btn_arrow">
+											<span className={`${styles.arrow_one} arrow_one`}>
+												<img src={arrow.src} />
+											</span>
+											<span className={`${styles.arrow_two} arrow_two`}>
+												<img src={arrow.src} />
+											</span>
+										</button>
+									</a>
+								</div>
+								<p className="paraTxt color_white">
+									Operational excellence is offered via FEC management contract. Our
+									expert team manages day-to-day operations, allowing clients to focus on
+									strategic goals.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className={`${styles.yellow_main} pt_100`}>
+					<Slider {...settings}>
+						<div className={`${styles.yellow_item}`}>
+							<div className={`${styles.yellow_box} f_j`}>
+								<div className="dot_two dots_p"></div>
+								<div className="dot_three dots_p"></div>
+								<div className={`${styles.yellow_content}`}>
+									<h3 className="text_35 pb_20">
+										Elevate your home with custom entertainment solutions
+									</h3>
+									<p className="paraTxt">
+										Lorem Ipsum is simply dummy text of the printing and typesetting
+										industry. Lorem Ipsum has been the industry's standard dummy text ever
+										since the 1500s
+									</p>
+									<div className={`${styles.yellow_btn} pt_30`}>
+										<a href="#" rel="noreferrer">
+											<button className="btn_project_default yellow_btn">
+												Learn More
+											</button>
+										</a>
+									</div>
+								</div>
+								<div className={`${styles.yellow_img}`}>
+									<img className="" src={yellow_img.src} alt="img" />
+								</div>
+							</div>
+						</div>
+
+						<div className={`${styles.yellow_item}`}>
+							<div className={`${styles.yellow_box} f_j`}>
+								<div className="dot_two dots_p"></div>
+								<div className="dot_three dots_p"></div>
+								<div className={`${styles.yellow_content}`}>
+									<h3 className="text_35 pb_20">
+										Elevate your home with custom entertainment solutions
+									</h3>
+									<p className="paraTxt">
+										Lorem Ipsum is simply dummy text of the printing and typesetting
+										industry. Lorem Ipsum has been the industry's standard dummy text ever
+										since the 1500s
+									</p>
+									<div className={`${styles.yellow_btn} pt_30`}>
+										<a href="#" rel="noreferrer">
+											<button className="btn_project_default yellow_btn">
+												Learn More
+											</button>
+										</a>
+									</div>
+								</div>
+								<div className={`${styles.yellow_img}`}>
+									<img className="" src={yellow_img.src} alt="img" />
+								</div>
+							</div>
+						</div>
+						<div className={`${styles.yellow_item}`}>
+							<div className={`${styles.yellow_box} f_j`}>
+								<div className="dot_two dots_p"></div>
+								<div className="dot_three dots_p"></div>
+								<div className={`${styles.yellow_content}`}>
+									<h3 className="text_35 pb_20">
+										Elevate your home with custom entertainment solutions
+									</h3>
+									<p className="paraTxt">
+										Lorem Ipsum is simply dummy text of the printing and typesetting
+										industry. Lorem Ipsum has been the industry's standard dummy text ever
+										since the 1500s
+									</p>
+									<div className={`${styles.yellow_btn} pt_30`}>
+										<a href="#" rel="noreferrer">
+											<button className="btn_project_default yellow_btn">
+												Learn More
+											</button>
+										</a>
+									</div>
+								</div>
+								<div className={`${styles.yellow_img}`}>
+									<img className="" src={yellow_img.src} alt="img" />
+								</div>
+							</div>
+						</div>
+					</Slider>
+				</div>
+			</div>
+		</section>
+	);
 }
--- src/sections/home/ValuedDistributors.js
+++ src/sections/home/ValuedDistributors.js
@@ -12,7 +12,6 @@ import "slick-carousel/slick/slick-theme.css";
 // STYLES //
 import styles from "../../styles/sections/home/ValuedDistributors.module.scss";
 
-
 // IMAGES //
 import sodikart from "../../../public/img/home/sodikart.png";
 import sega_logo from "../../../public/img/home/sega_logo.png";
@@ -23,15 +22,14 @@ import lai_logo from "../../../public/img/home/lai_logo.png";
 import funshare from "../../../public/img/home/funshare.png";
 import cheer from "../../../public/img/home/cheer.png";
 
-
 /** Home Hero Section */
 export default function ValuedDistributors() {
-  var settings = {
+	var settings = {
 		dots: true,
 		arrows: false,
 		infinite: true,
 		slidesToShow: 4,
-    slidesToScroll: 3,
+		slidesToScroll: 3,
 		autoplay: true,
 		pauseOnHover: false,
 		speed: 1000,
@@ -40,7 +38,7 @@ export default function ValuedDistributors() {
 				breakpoint: 767,
 				settings: {
 					slidesToShow: 2,
-          slidesToScroll: 2,
+					slidesToScroll: 2,
 				},
 			},
 		],
@@ -57,46 +55,47 @@ export default function ValuedDistributors() {
 	};
 
 	return (
-    <section className={`${styles.valued_distributors_wrap} pb_100`}>
-      <div className="container">
-        <div className={`${styles.section_title}`}>
-          <h2 className="heading_text_40 pb_20">Our valued distributors </h2>
-        </div>
-        <div className={`${styles.valued_distributors_border}`}>
-          <div className="dot_one dots_p"></div>
-          <div className="dot_two dots_p"></div>
-          <div className="dot_three dots_p"></div>
-          <div className={`${styles.valued_logo}`}>
-            <Slider {...settings}>
-              <div className={`${styles.valued_item}`}>
-                <img className="" src={sodikart.src} alt="logo" />
-              </div>
-              <div className={`${styles.valued_item}`}>
-                <img className="" src={sega_logo.src} alt="logo" />
-              </div>
-              <div className={`${styles.valued_item}`}>
-                <img className="" src={brunswick.src} alt="logo" />
-              </div>
-              <div className={`${styles.valued_item}`}>
-                <img className="" src={tm_logo.src} alt="logo" />
-              </div>
-              <div className={`${styles.valued_item}`}>
-                <img className="" src={raw_logo.src} alt="logo" />
-              </div>
-              <div className={`${styles.valued_item}`}>
-                <img className="" src={lai_logo.src} alt="logo" />
-              </div>
-              <div className={`${styles.valued_item}`}>
-                <img className="" src={funshare.src} alt="logo" />
-              </div>
-              <div className={`${styles.valued_item}`}>
-                <img className="" src={cheer.src} alt="logo" />
-              </div>
-            </Slider>
-          </div>
-        </div>
-      </div>
-
-    </section>
-  );
+		<section className={`${styles.valued_distributors_wrap} pb_100`}>
+			<div className="container">
+				<div className={`${styles.section_title}`}>
+					<h2 className="heading_text_40 pb_20">Our valued distributors </h2>
+				</div>
+				<div
+					className={`${styles.valued_distributors_border} border_animation white_border_animation`}
+				>
+					<div className="dot_one dots_p"></div>
+					<div className="dot_two dots_p"></div>
+					<div className="dot_three dots_p"></div>
+					<div className={`${styles.valued_logo}`}>
+						<Slider {...settings}>
+							<div className={`${styles.valued_item}`}>
+								<img className="" src={sodikart.src} alt="logo" />
+							</div>
+							<div className={`${styles.valued_item}`}>
+								<img className="" src={sega_logo.src} alt="logo" />
+							</div>
+							<div className={`${styles.valued_item}`}>
+								<img className="" src={brunswick.src} alt="logo" />
+							</div>
+							<div className={`${styles.valued_item}`}>
+								<img className="" src={tm_logo.src} alt="logo" />
+							</div>
+							<div className={`${styles.valued_item}`}>
+								<img className="" src={raw_logo.src} alt="logo" />
+							</div>
+							<div className={`${styles.valued_item}`}>
+								<img className="" src={lai_logo.src} alt="logo" />
+							</div>
+							<div className={`${styles.valued_item}`}>
+								<img className="" src={funshare.src} alt="logo" />
+							</div>
+							<div className={`${styles.valued_item}`}>
+								<img className="" src={cheer.src} alt="logo" />
+							</div>
+						</Slider>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
