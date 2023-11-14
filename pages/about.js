cba9a6a4493c124421453926c438dfeaf9696652
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2023-11-14 17:41:06 +0530

    about page desktop done

--- pages/about.js
+++ pages/about.js
@@ -47,7 +47,7 @@ export default function About() {
 			<OurVision />
 			<AboutFeedback />
 			<ExcellenceEntertainment />
-			{/* <Luminaries /> */}
+			<Luminaries />
 			
 			</main>
 			<Footer />
