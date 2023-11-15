c18b6099056daaf8bb6ee7526d2a5f530e288419
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2023-11-15 14:09:03 +0530

    about us page responsive done

--- src/sections/about/AboutFeedback.js
+++ src/sections/about/AboutFeedback.js
@@ -23,7 +23,7 @@ export default function AboutFeedback() {
           <img src={quate.src} alt="img" />
 					<h2 className="heading_text_55 pb_20 pt_10">
             Lorem ipsum 
-            <br className="hidden-xs" />dolor sit amet
+            <br className="hidden-xs" /> dolor sit amet
 					</h2>
           <p className="paraTxt_18 color_white">With a single-minded focus, I began my journey with a mission to transform entertainment places, and today I am glad to say that every bowling and amusement center CSML has developed stands and shines successfully regardless of the years. CSML strives to deliver entertainment with absolute honesty, value, and quality in order to maintain goodwill for us and our clients.</p>
 				</div>
