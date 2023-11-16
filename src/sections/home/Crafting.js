5b8e0a247ed10722a502d60672ed8cc3e6834559
VipVishal007 <mvishu405@gmail.com>
2023-11-17 00:20:34 +0530

    crating and about page animation

--- src/sections/home/Crafting.js
+++ src/sections/home/Crafting.js
@@ -1,4 +1,8 @@
 // MODULES //
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "../../../node_modules/gsap/ScrollTrigger";
 
 // COMPONENTS //
 
@@ -25,36 +29,63 @@ import play_icon from "../../../public/img/play_icon.svg";
 
 /** Home Hero Section */
 export default function Crafting() {
+	useEffect(() => {
+		setTimeout(() => {
+			gsap.registerPlugin(ScrollTrigger);
+			const cricle_box = document.querySelector(".cricle_box").offsetLeft;
+			const crafting_heading =
+				document.querySelector(".crafting_heading").offsetWidth;
+			const totalBoxW = cricle_box + crafting_heading + 150;
+			console.log(cricle_box);
+			console.log(crafting_heading);
+			const winW = window.innerWidth;
+			const bannerPin = gsap.timeline({
+				scrollTrigger: {
+					trigger: ".crafting_wrap",
+					start: "top center",
+					end: "top top",
+					pin: false,
+					pinSpacing: true,
+					scrub: true,
+					// markers: false,
+				},
+			});
+			bannerPin.to(".cricle_box", { left: totalBoxW, rotate: 360 }, 0);
+		}, 500);
+	}, []);
 	return (
-		<section className={`${styles.crafting_wrap} ptb_100`}>
-			<div className="container">
-				<div className={`${styles.crafting_flex} d_f`}>
-					<div className={`${styles.cricle_box}`}>
-						<img className="" src={cricle_img.src} />
-					</div>
-
-					<div className={`${styles.left_content}`}>
-						<h2 className="text_xxxxxI color_white pb_40">Crafting</h2>
-						<p className="paraTxt color_white">
-							Since our establishment in 2002, we have cultivated an extensive
-							portfolio of entertainment solutions. Additionally, we've been the
-							exclusive distributor for Brunswick Bowling Products USA in India &
-							SAARC.{" "}
-						</p>
-					</div>
-					<div className={`${styles.right_content} pt_40`}>
-						<h2 className="heading_text_45 color_white pb_60">
-							world-class entertainment <br />
-							hubs for over 50 years
-						</h2>
-						<div className={`${styles.btn_height}`}>
-							<a href="#" rel="noreferrer">
-								<button className="btn_project_default color_white">About Us</button>
-							</a>
+		<section className={`${styles.crafting_wrap} ptb_100 crafting_wrap`}>
+			<section className={`${styles.crafting_section} crafting_section`}>
+				<div className={`${styles.cricle_box} cricle_box`}>
+					<img className="" src={cricle_img.src} />
+				</div>
+				<div className="container box_left_space">
+					<div className={`${styles.crafting_flex} d_f`}>
+						<div className={`${styles.left_content}`}>
+							<h2 className="text_xxxxxI color_white pb_40 crafting_heading">
+								Crafting
+							</h2>
+							<p className="paraTxt color_white">
+								Since our establishment in 2002, we have cultivated an extensive
+								portfolio of entertainment solutions. Additionally, we've been the
+								exclusive distributor for Brunswick Bowling Products USA in India &
+								SAARC.{" "}
+							</p>
+						</div>
+						<div className={`${styles.right_content}`}>
+							<h2 className="heading_text_45 color_white pb_60">
+								world-class entertainment <br />
+								hubs for over 50 years
+							</h2>
+							<div className={`${styles.btn_height}`}>
+								<a href="#" rel="noreferrer">
+									<button className="btn_project_default color_white">About Us</button>
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</section>
 
 			<div className="container pt_100">
 				<div className={`${styles.intertainment_border} border_animation`}>
@@ -112,7 +143,10 @@ export default function Crafting() {
 							<LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgVideo]}>
 								<a href="https://youtu.be/75yhYazRwRs?feature=shared">
 									<div className={`${styles.video_box}`}>
-										<img className={`${styles.video_img} border_12 img`}src={video_img.src} />
+										<img
+											className={`${styles.video_img} border_12 img`}
+											src={video_img.src}
+										/>
 										<div className={`${styles.video_play}`}>
 											<img
 												src={play_icon.src}
