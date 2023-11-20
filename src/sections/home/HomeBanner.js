0f15d64199aa86a4fc7d5c1236afd5e9ac1e59a3
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2023-11-20 17:01:49 +0530

    changes

--- src/sections/home/HomeBanner.js
+++ src/sections/home/HomeBanner.js
@@ -35,31 +35,33 @@ export default function HomeBanner() {
   }, []);
   
 	return (
-    <section className={`${styles.home_banner_wrap} home_banner_top`}>
-      <div className={`${styles.banner_img}`}>
-        <img className="width_100" src={home_banner.src} />
-        {/* <video autoPlay muted loop className={styles.video_box}>         
-          <source src="./img/home/movie.mp4" type="video/mp4"/>       
-        </video> */}
-      </div>
-      <div className={`${styles.banner_content}`}>
-        <h1 className="heading_text_70 color_white pb_40">The architects of amusement </h1>
-        <p className="paraTxt color_white">Turning entertainment dreams into reality with premium amusement solutions</p>
-      </div>
-      <div className={`${styles.circle_right}`} >
+    <section className={`${styles.home_banner_wrap}`}>
+      <div className={`${styles.home_banner_two} home_banner_top`}>
+        <div className={`${styles.banner_img}`}>
+          <img className="width_100" src={home_banner.src} />
+          {/* <video autoPlay muted loop className={styles.video_box}>         
+            <source src="./img/home/movie.mp4" type="video/mp4"/>       
+          </video> */}
+        </div>
+        <div className={`${styles.banner_content}`}>
+          <h1 className="heading_text_70 color_white pb_40">The architects of amusement </h1>
+          <p className="paraTxt color_white">Turning entertainment dreams into reality with premium amusement solutions</p>
+        </div>
+        <div className={`${styles.circle_right}`} >
+          
+          <ScrollSection
+            
+            
+            to="crafting_section"
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={500}
+          >
+            <img className="" src={cricle_arrow.src} />
+          </ScrollSection>
         
-        <ScrollSection
-          
-          
-          to="crafting_section"
-          spy={true}
-          smooth={true}
-          offset={-50}
-          duration={500}
-        >
-          <img className="" src={cricle_arrow.src} />
-        </ScrollSection>
-       
+        </div>
       </div>
     </section>
   );
