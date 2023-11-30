a437de6361791505c545c49eea0e8555f0d9138b
sagar-cr7 <sagarvb7@gmail.com>
2023-11-30 14:34:43 +0530

    +

---
+++ src/components/Cursor.js
@@ -0,0 +1,14 @@
+// MODULES //
+import React, { useState, useEffect } from "react";
+// COMPONENTS //
+// SECTIONS //
+// PLUGINS //
+// STYLES //
+// IMAGES //
+/** Footer Component */
+
+const Cursor = (showCursor) => {
+	return <div>Cursor</div>;
+};
+
+export default Cursor;
