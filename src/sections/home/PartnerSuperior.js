cf05574617c65f3e7bafd6f15d91d61bc7ef6a3c
VipVishal007 <mvishu405@gmail.com>
2023-12-07 17:23:44 +0530

    partners slider backend

--- src/sections/home/PartnerSuperior.js
+++ src/sections/home/PartnerSuperior.js
@@ -19,7 +19,7 @@ import yellow_img from "../../../public/img/home/yellow_img.jpg";
 import arrow from "../../../public/img/arrow.svg";
 
 /** Home Hero Section */
-export default function PartnerSuperior() {
+export default function PartnerSuperior({ data }) {
 	var settings = {
 		dots: true,
 		arrows: false,
@@ -31,9 +31,11 @@ export default function PartnerSuperior() {
 		speed: 2000,
 		autoplaySpeed: 5000,
 	};
-
+	console.log(data);
 	return (
-		<section className={`${styles.partner_superior_wrap} dot_animation_box ptb_100`}>
+		<section
+			className={`${styles.partner_superior_wrap} dot_animation_box ptb_100`}
+		>
 			<div className="container">
 				<div className={`${styles.section_title} pb_40 toTop`} data-scroll>
 					<h2 className="heading_text_55 color_white pb_20">
@@ -48,14 +50,15 @@ export default function PartnerSuperior() {
 				</div>
 
 				<div className={`${styles.partner_superior_flex} f_j`}>
-					<div className={`${styles.partner_superior_box} commonBorderAnimation toTop`} data-scroll>
+					<div
+						className={`${styles.partner_superior_box} commonBorderAnimation toTop`}
+						data-scroll
+					>
 						<div className={`${styles.partner_superior_content}`}>
 							<img className="border_8" src={partner_img.src} alt="img" />
 							<div className={`${styles.desc_box}`}>
 								<div className={`${styles.desc_title} f_j`}>
-									<h3 className="text_24 color_white text_500 ">
-										Consultancy Services
-									</h3>
+									<h3 className="text_24 color_white text_500 ">Consultancy Services</h3>
 									<a href="#" rel="noreferrer">
 										<button className="btn_arrow">
 											<span className={`${styles.arrow_one} arrow_one`}>
@@ -75,14 +78,16 @@ export default function PartnerSuperior() {
 							</div>
 						</div>
 					</div>
-					<div className={`${styles.partner_superior_box} commonBorderAnimation toTop`} data-scroll>
+					<div
+						className={`${styles.partner_superior_box} commonBorderAnimation toTop`}
+						data-scroll
+					>
 						<div className={`${styles.partner_superior_content}`}>
 							<img className="border_8" src={partner_img_two.src} alt="img" />
 							<div className={`${styles.desc_box}`}>
 								<div className={`${styles.desc_title} f_j`}>
 									<h3 className="text_24 color_white text_500">
-										Management Contract - 
-										Operational Excellence
+										Management Contract - Operational Excellence
 									</h3>
 									<a href="#" rel="noreferrer">
 										<button className="btn_arrow">
@@ -107,36 +112,39 @@ export default function PartnerSuperior() {
 
 				<div className={`${styles.yellow_main} pt_100 toTop`} data-scroll>
 					<Slider {...settings}>
-						<div className={`${styles.yellow_item}`}>
-							<div className={`${styles.yellow_box} f_j`}>
-								<div className="dot_two dots_p"></div>
-								<div className="dot_three dots_p"></div>
-								<div className={`${styles.yellow_content}`}>
-									<h3 className="text_35 pb_20">
-										Elevate your home with custom entertainment solutions
-									</h3>
-									<p className="paraTxt opacity_one">
-										Lorem Ipsum is simply dummy text of the printing and typesetting
-										industry. Lorem Ipsum has been the industry's standard dummy text ever
-										since the 1500s
-									</p>
-									<div className={`${styles.yellow_btn} pt_30`}>
-										<a href="#" rel="noreferrer">
-											<span className="span_btn yellow_btn">
-												<button className="btn_project_default">
-													Learn More
-												</button>
-											</span>
-										</a>
+						{data.map((item) => {
+							return (
+								<div
+									className={`${styles.yellow_item}`}
+									key={item.attributes.PartnersHeading}
+								>
+									<div className={`${styles.yellow_box} f_j`}>
+										<div className="dot_two dots_p"></div>
+										<div className="dot_three dots_p"></div>
+										<div className={`${styles.yellow_content}`}>
+											<h3 className="text_35 pb_20">{item.attributes.PartnersHeading}</h3>
+											<p className="paraTxt opacity_one">{item.attributes.PartnersPara}</p>
+											<div className={`${styles.yellow_btn} pt_30`}>
+												<a href={item.attributes.PartnersURL} rel="noreferrer">
+													<span className="span_btn yellow_btn">
+														<button className="btn_project_default">Learn More</button>
+													</span>
+												</a>
+											</div>
+										</div>
+										<div className={`${styles.yellow_img}`}>
+											<img
+												className="border_20"
+												src={`${process.env.NEXT_PUBLIC_STRAPI_DO_BASE_URL}${item.attributes.PartnersImg.data.attributes.url}`}
+												alt="img"
+											/>
+										</div>
 									</div>
 								</div>
-								<div className={`${styles.yellow_img}`}>
-									<img className="border_20" src={yellow_img.src} alt="img" />
-								</div>
-							</div>
-						</div>
+							);
+						})}
 
-						<div className={`${styles.yellow_item}`}>
+						{/* <div className={`${styles.yellow_item}`}>
 							<div className={`${styles.yellow_box} f_j`}>
 								<div className="dot_two dots_p"></div>
 								<div className="dot_three dots_p"></div>
@@ -152,9 +160,7 @@ export default function PartnerSuperior() {
 									<div className={`${styles.yellow_btn} pt_30`}>
 										<a href="#" rel="noreferrer">
 											<span className="span_btn yellow_btn">
-												<button className="btn_project_default">
-													Learn More
-												</button>
+												<button className="btn_project_default">Learn More</button>
 											</span>
 										</a>
 									</div>
@@ -180,9 +186,7 @@ export default function PartnerSuperior() {
 									<div className={`${styles.yellow_btn} pt_30`}>
 										<a href="#" rel="noreferrer">
 											<span className="span_btn yellow_btn">
-												<button className="btn_project_default">
-													Learn More
-												</button>
+												<button className="btn_project_default">Learn More</button>
 											</span>
 										</a>
 									</div>
@@ -191,7 +195,7 @@ export default function PartnerSuperior() {
 									<img className="border_20" src={yellow_img.src} alt="img" />
 								</div>
 							</div>
-						</div>
+						</div> */}
 					</Slider>
 				</div>
 			</div>
