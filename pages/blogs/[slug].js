09228fd559340c960e6e33551e417f033d9a63ae
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2024-01-11 15:35:52 +0530

    chnages done

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -47,6 +47,54 @@ export default function BlogInside({ BlogData, RelatedBlogData }) {
 		});
 	}, []);
 	const mainDate = BlogData?.BlogDate;
+
+	var settings = {
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		autoplay: true,
+		pauseOnHover: false,
+		speed: 1000,
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+		afterChange: (i) => {
+			fillProgress(i + 1);
+		},
+		// prevArrow: (
+		// 	<button className={`${styles.prevarrow} about_prevarrow`}>
+		// 		<Image src={arrow_slider.src} alt="arrow" width={40} height={40} />
+		// 	</button>
+		// ),
+		// nextArrow: (
+		// 	<button className={`${styles.nextarrow} about_nextarrow`}>
+		// 		<Image src={arrow_slider.src} alt="arrow" width={40} height={40} />
+		// 	</button>
+		// ),
+	};
+
+	const [progressWidth, setProgressWidth] = useState(0);
+	useEffect(() => {
+		fillProgress(1);
+	}, []);
+	const fillProgress = (currInd) => {
+		setProgressWidth((currInd / RelatedBlogData.length) * 100);
+	};
 	return (
 		<div>
 			<Header />
@@ -92,43 +140,54 @@ export default function BlogInside({ BlogData, RelatedBlogData }) {
 				<section className={`${styles.blog_main_section} pt_80 pb_40`}>
 					<div className="container">
 						<h2 className="heading_text_55 text_center">Others Blog & Articles</h2>
-						<div className={`${styles.blog_info_sec} f_w_j pt_50`}>
-							{RelatedBlogData?.map((item) => {
-								const dateString = item.attributes.BlogDate;
-								const formattedDate = format(new Date(dateString), "dd MMMM yyyy");
-								return (
-									<div
-										className={`${styles.blog_info} commonBorderAnimation whiteCommonBorderAnimation mb_80`}
-									>
-										<div className={`${styles.blog_info_style}`}>
-											<img
-												src={`${process.env.NEXT_PUBLIC_STRAPI_DO_BASE_URL}${item.attributes.BlogThumb?.data.attributes.url}`}
-												alt="Blog Img"
-												className="b_r_8"
-											/>
+						<div className={`${styles.blog_info_sec} pt_50`}>
+							<Slider {...settings}>
+								{RelatedBlogData?.map((item) => {
+									const dateString = item.attributes.BlogDate;
+									const formattedDate = format(new Date(dateString), "dd MMMM yyyy");
+									return (
+										<div
+											className={`${styles.blog_info} commonBorderAnimation whiteCommonBorderAnimation mb_30`}
+										>
+											<div className={`${styles.blog_info_style}`}>
+												<img
+													src={`${process.env.NEXT_PUBLIC_STRAPI_DO_BASE_URL}${item.attributes.BlogThumb?.data.attributes.url}`}
+													alt="Blog Img"
+													className="b_r_8"
+												/>
+											</div>
+											<div className={`${styles.blog_info_style}`}>
+												<h6 className="paraTxt_16 color_black_opacity pt_20 text_500">
+													{/* {item.attributes.BlogDate} */}
+													{formattedDate}
+												</h6>
+												<h4 className="text_24 text_500 pt_10 pb_30">
+													{item.attributes.BlogHeading}
+												</h4>
+												<a href={`/blogs/${item.attributes.BlogSlug}`} rel="noreferrer">
+													<button className="btn_arrow">
+														<span className={`${styles.arrow_one} arrow_one`}>
+															<img src={arrow.src} />
+														</span>
+														<span className={`${styles.arrow_two} arrow_two`}>
+															<img src={arrow.src} />
+														</span>
+													</button>
+												</a>
+											</div>
 										</div>
-										<div className={`${styles.blog_info_style}`}>
-											<h6 className="paraTxt_16 color_black_opacity pt_20 text_500">
-												{/* {item.attributes.BlogDate} */}
-												{formattedDate}
-											</h6>
-											<h4 className="text_24 text_500 pt_10 pb_30">
-												{item.attributes.BlogHeading}
-											</h4>
-											<a href={`/blogs/${item.attributes.BlogSlug}`} rel="noreferrer">
-												<button className="btn_arrow">
-													<span className={`${styles.arrow_one} arrow_one`}>
-														<img src={arrow.src} />
-													</span>
-													<span className={`${styles.arrow_two} arrow_two`}>
-														<img src={arrow.src} />
-													</span>
-												</button>
-											</a>
-										</div>
-									</div>
-								);
-							})}
+									);
+								})}
+							</Slider>
+							
+						</div>
+						<div className={`${styles.progress_div} pb_100`}>
+							<div className={`${styles.progress_bar}`}>
+								<div
+									style={{ width: `${progressWidth}%` }}
+									className={`${styles.color_div}`}
+								></div>
+							</div>
 						</div>
 					</div>
 				</section>
