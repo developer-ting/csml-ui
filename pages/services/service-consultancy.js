0ebb4663bb8ddf517cc38b4e67de6650ef389788
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2024-01-12 21:47:52 +0530

    service page read more

--- pages/services/service-consultancy.js
+++ pages/services/service-consultancy.js
@@ -31,6 +31,12 @@ import Tabs_img3 from "../../public/img/services_consultancy/tab_img3.jpg";
 
 /** Services Page */
 export default function ServiceConsultancy() {
+  const [isShowMore, setIsShowMore] = useState(false);
+	/** toggleReadMoreLess function */
+	const toggleReadMoreLess = () => {
+    setIsShowMore(!isShowMore);
+  };
+
 	const [nav1, setNav1] = useState();
 	const [nav2, setNav2] = useState();
 	useEffect(() => {
@@ -116,7 +122,13 @@ export default function ServiceConsultancy() {
 								variableWidth={true}
 							>
 								<div className={`${styles.slide_item}`}>
+									<p>Turning Vision into Reality</p>
+								</div>
+								<div className={`${styles.slide_item}`}>
 									<p>Planning and Layout Design</p>
+								</div>
+								<div className={`${styles.slide_item}`}>
+									<p>Suggestive Game Mix</p>
 								</div>
 								<div className={`${styles.slide_item}`}>
 									<p>Exclusive Distribution</p>
@@ -147,27 +159,68 @@ export default function ServiceConsultancy() {
 							ref={(slider1) => setNav1(slider1)}
 						>
 							<div className={`${styles.slide_item_two}`}>
-									<div className={`${styles.tabs_bx}`}>
-										<div
-											className={`${styles.tabs_img}  border_animation white_border_animation`}
-										>
-											<img className="img border_8" src={Tabs_img1.src} />
-										</div>
-										<div className={`${styles.tabs_info}`}>
-											<p className={`${styles.info_title} font_secondary`}>
-												Planning and Layout Design
-											</p>
-											<p
-												className={`${styles.info_desc} paraTxt_18 font_secondary opacity_one`}
-											>
-												Our expertise in optimizing available space transforms it into a
-												bespoke arcade amusement hub. We provide specialized layout plans
-												designed to suit your location, complete with electrical layout
-												plans, operating manuals, and ongoing support.
-											</p>
-										</div>
+								<div className={`${styles.tabs_bx}`}>
+									<div
+										className={`${styles.tabs_img}  border_animation white_border_animation`}
+									>
+										<img className="img border_8" src={Tabs_img1.src} />
 									</div>
+									<div className={`${styles.tabs_info}`}>
+										<p className={`${styles.info_title} font_secondary`}>
+										 Turning Vision into Reality
+										</p>
+										<p
+											className={`${styles.info_desc} paraTxt_18 font_secondary opacity_one`}
+										>
+											Our expertise in optimizing available space transforms it into a
+											bespoke arcade amusement hub. We provide specialized layout plans
+											designed to suit your location, complete with electrical layout
+											plans, operating manuals, and ongoing support.
+											
+										</p>
+									</div>
+								</div>
 							</div>
+							<div className={`${styles.slide_item_two}`}>
+								<div className={`${styles.tabs_bx}`}>
+									<div
+										className={`${styles.tabs_img}  border_animation white_border_animation`}
+									>
+										<img className="img border_8" src={Tabs_img1.src} />
+									</div>
+									<div className={`${styles.tabs_info}`}>
+										<p className={`${styles.info_title} font_secondary`}>
+											Planning and Layout Design
+										</p>
+										<p
+											className={`${styles.info_desc} paraTxt_18 font_secondary opacity_one`}
+										>
+											Our expertise in optimizing available space transforms it into a bespoke arcade amusement hub. We provide specialized layout plans designed to suit your location, complete with electrical layout plans, operating manuals, and ongoing support.
+										</p>
+									</div>
+								</div>
+							</div>
+
+							<div className={`${styles.slide_item_two}`}>
+								<div className={`${styles.tabs_bx}`} >
+									<div
+										className={`${styles.tabs_img}  border_animation white_border_animation`}
+									>
+										<img className="img border_8" src={Tabs_img2.src} />
+									</div>
+									<div className={`${styles.tabs_info}`}>
+										<p className={`${styles.info_title} font_secondary`}>
+											Suggestive Game Mix
+										</p>
+										<p
+											className={`${styles.info_desc} paraTxt_18 font_secondary opacity_one`}
+										>
+											At CSML, we go beyond providing amusement equipment; we craft unforgettable experiences. Our approach is rooted in the thorough consideration of your target audience's demographics, age groups, spending capacity, and budget range. We take pride in offering a selection of amusement equipment that not only drives high revenue but also resonates with the interests of your guests.
+										</p>
+									</div>
+								</div>
+							</div>
+
 							<div className={`${styles.slide_item_two}`}>
 									<div className={`${styles.tabs_bx}`} >
 										<div
@@ -187,6 +240,7 @@ export default function ServiceConsultancy() {
 										</div>
 									</div>
 							</div>
+
 							<div className={`${styles.slide_item_two}`}>
 									<div className={`${styles.tabs_bx}`}>
 										<div
@@ -201,7 +255,7 @@ export default function ServiceConsultancy() {
 											<p
 												className={`${styles.info_desc} paraTxt_18 font_secondary opacity_one`}
 											>
-												At CSML, we specialize in curating a diverse mix of entertainment equipment tailored to high-thrill and family-friendly experiences. We prioritize creating unforgettable experiences by analyzing your audience's demographics, age groups, and budget considerations. Our goal is to maximize revenue while appealing to your guests' specific interests.
+												With our expertise, we assist you in selecting the ideal blend of amusement equipment, catering to high-thrill experiences and family-friendly attractions. This guarantees that your entertainment center becomes a town favorite, leaving visitors with cherished memories.
 											</p>
 										</div>
 									</div>
@@ -220,10 +274,7 @@ export default function ServiceConsultancy() {
 											<p
 												className={`${styles.info_desc} paraTxt_18 font_secondary opacity_one`}
 											>
-												Our expertise in optimizing available space transforms it into a
-												bespoke arcade amusement hub. We provide specialized layout plans
-												designed to suit your location, complete with electrical layout
-												plans, operating manuals, and ongoing support.
+												We simplify the entire process of obtaining amusement equipment from worldwide suppliers. From overseeing ocean freight to managing import clearance, we ensure a seamless journey for the equipment to reach your location.
 											</p>
 										</div>
 									</div>
@@ -237,12 +288,12 @@ export default function ServiceConsultancy() {
 										</div>
 										<div className={`${styles.tabs_info}`}>
 											<p className={`${styles.info_title} font_secondary`}>
-											Installation & Training
+												Installation & Training
 											</p>
 											<p
 												className={`${styles.info_desc} paraTxt_18 font_secondary opacity_one`}
 											>
-												What sets us apart is our exclusive distribution of industry-leading brands. Our substantial buying power and involvement in multiple projects enable us to offer competitive prices without compromising on quality. This ensures that you receive top-notch amusement equipment that aligns with your budgetary requirements.
+												Our skilled installers take charge of setting up and thoroughly testing the amusement equipment, ensuring flawless operation. Furthermore, our comprehensive training program empowers your staff with hands-on experience in operating the machines and equips them with the essential knowledge to ensure seamless venue operations.
 											</p>
 										</div>
 									</div>
@@ -261,8 +312,109 @@ export default function ServiceConsultancy() {
 											<p
 												className={`${styles.info_desc} paraTxt_18 font_secondary opacity_one`}
 											>
-												At CSML, we specialize in curating a diverse mix of entertainment equipment tailored to high-thrill and family-friendly experiences. We prioritize creating unforgettable experiences by analyzing your audience's demographics, age groups, and budget considerations. Our goal is to maximize revenue while appealing to your guests' specific interests.
+												At CSML, our dedication to your success extends beyond just consultancy and equipment. With our Pre & Post Operation Support services, we ensure that your operations run seamlessly by providing the essential resources and guidance you need.
 											</p>
+											{isShowMore && (
+													<div className="pt_20">
+														<h4 className="paraTxt_18 font_secondary text_500">
+															Recruitment Checklist
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Our detailed staffing guide helps you assemble the right team, ensuring efficiency and excellence in your operations.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+														 Pricing Structure
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Optimize your revenue with our expert suggestions for pricing games, activities, and party packages.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+														 Operational Supplies
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Stay stocked and prepared with our comprehensive list of redemption tickets, gifts, and spares.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+														 Gift Categories
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Enhance the customer experience with our curated gift category recommendations, adding that extra touch to your center.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+														 Pay-out Settings
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Fine-tune prize/ticket game payout settings with expert guidance to ensure optimal player engagement and operational efficiency.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+														Checklist Creation
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Efficiently manage your operations with our comprehensive checklists, covering everything from daily operations to asset management, maintenance, housekeeping, and more.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+															Debit Card System
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Equip your team for success with thorough Debit Card system training, catering to back-office owners, managers, cashiers, and redemption staff.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+														  Redemption Counter
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Enhance customer satisfaction by setting up an efficient redemption counter with our assistance.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+															On-Job Training
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															We provide a 7-day training program to assist your team in achieving smooth operations and equipment maintenance.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+															SOPs (Standard Operating Procedures)
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															We offer a comprehensive set of Standard Operating Procedures to streamline your operations further.
+														</p>
+														<h4 className="paraTxt_18 font_secondary text_500 pt_10">
+															Post-Opening Support
+														</h4>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															For two months following your center's grand opening, we offer expert guidance and support to ensure your success.
+														</p>
+														<p
+															className={`${styles.info_desc} text_xs font_secondary opacity_one`}
+														>
+															Partner with us for a successful journey in the world of entertainment centers. Your success is our unwavering commitment.
+														</p>
+													</div>
+												)}
+
+												<a className="pt_20 paraTxt_18 font_secondary opacity_one" onClick={toggleReadMoreLess}>
+													{isShowMore ? "Read Less" : "Read More"}
+												</a>
 										</div>
 									</div>
 							</div>
