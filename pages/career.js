98406e63bb439aeb72a900b1e92a8c76db9dffaf
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2024-02-19 12:25:14 +0530

    career page testimonial remove

--- pages/career.js
+++ pages/career.js
@@ -47,7 +47,7 @@ export default function Career() {
 				<CareerSuccess />
 				<LifeCsml />
 				<OurGuidingValues />
-				<CsmlExperience />
+				{/* <CsmlExperience /> */}
 				<HowToApply />
 			</main>
 			<Footer />
