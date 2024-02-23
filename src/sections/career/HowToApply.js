7305a4b561798171d6e3e8cfa86dff158bbc7bef
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2024-02-23 13:02:06 +0530

    career page form done

--- src/sections/career/HowToApply.js
+++ src/sections/career/HowToApply.js
@@ -1,6 +1,6 @@
 // MODULES //
 import { useEffect, useState } from "react";
-
+import { useForm } from "react-hook-form";
 // COMPONENTS //
 
 // SECTIONS //
@@ -18,6 +18,41 @@ import how_to_apply from "../../../public/img/career/how_to_apply.jpg";
 
 /** Home Hero Section */
 export default function HowToApply() {
+  const [isSubmited, setIsSubmited] = useState(false);
+
+  const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm();
+
+  const onSubmit = async (data) => {
+		const Headers = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`,
+			},
+			body: JSON.stringify({
+				data: data,
+			}),
+		};
+
+		async function sendData() {
+			await fetch(
+				`${process.env.NEXT_PUBLIC_STRAPI_DO_BASE_URL}/api/career-page-forms`,
+				Headers
+			)
+				.then((data) => data.json())
+				.then((data) => {
+					reset(), setIsSubmited(true);
+				})
+				.catch((err) => console.log(err));
+		}
+
+		sendData();
+	};
   
 	return (
 		<section className={`${styles.how_apply_wrap} ptb_100`}>
@@ -36,16 +71,149 @@ export default function HowToApply() {
             <p className={`${styles.info_para} paraTxt_18 text_400 pb_10 toTop`} data-scroll>
             In your cover letter, state your reasons for joining and how you can contribute. Alternatively, explore our job openings below.
             </p>
-            <div className={`${styles.btn_box} pt_30 toTop`} data-scroll>
+            {/* <div className={`${styles.btn_box} pt_30 toTop`} data-scroll>
               <a href="#" rel="noreferrer">
                 <span className="span_btn white_btn">
                   <button className="btn_project_default">Explore Opportunities</button>
                 </span>
               </a>
-            </div>
+            </div> */}
           </div>
-          <div className={`${styles.img_bx} border_animation white_border_animation toTop`} data-scroll>
-            <img className="img border_8" src={how_to_apply.src} />
+          <div className={`${styles.img_bx} toTop`} data-scroll>
+            {/* <img className="img border_8" src={how_to_apply.src} /> */}
+            <div className={`${styles.contact_form}`}>
+              <form
+                className={`${styles.form_main}`}
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <div className={`${styles.form_field}`}>
+                  <input
+                    className=""
+                    type="text"
+                    name="Name"
+                    placeholder="Name *"
+                    {...register("Name", {
+                      required: true,
+                      maxLength: 79,
+                    })}
+                  />
+                  {errors.Name && (
+                    <p className={`${styles.errors_msg}`}>This field is required</p>
+                  )}
+                </div>
+                <div className={`${styles.form_field}`}>
+                  <input
+                    className=""
+                    type="tel"
+                    name="phoneNumber"
+                    placeholder="Phone Number *"
+                    {...register("PhoneNumber", {
+                      required: true,
+                      pattern: /^[0-9]{10}$/i,
+                    })}
+                  />
+                  {errors.PhoneNumber && (
+                    <p className={`${styles.errors_msg}`}>This field is required</p>
+                  )}
+                </div>
+                <div className={`${styles.form_field}`}>
+                  <input
+                    className=""
+                    type="email"
+                    name="Email"
+                    placeholder="Email *"
+                    {...register("Email", {
+                      required: true,
+                      pattern: /^\S+@\S+$/i,
+                    })}
+                  />
+                  {errors.Email && (
+                    <p className={`${styles.errors_msg}`}>This field is required</p>
+                  )}
+                </div>
+                <div className={`${styles.form_field}`}>
+                  <select {...register("Position", { required: true })}>
+                    <option value="">Choose position  *</option>
+                    <option value="Accounts">Accounts</option>
+                    <option value="Sales">Sales</option>
+                    <option value="Marketing">Marketing </option>
+                    <option value="Technical">Technical </option>
+                    <option value="IT">IT </option>
+                    <option value="HR">HR </option>
+                    <option value="Administration">Administration </option>
+                    <option value="Others If Any pls specify">
+                      Others If Any pls specify
+                    </option>
+                  </select>
+                  {errors.Position && (
+                    <p className={`${styles.errors_msg}`}>This field is required</p>
+                  )}
+                </div>
+                <div className={`${styles.form_field}`}>
+                  <input
+                    className=""
+                    type="text"
+                    name="Estatus"
+                    placeholder="Specify employment Status"
+                    {...register("Status", {
+                      required: true,
+                      maxLength: 79,
+                    })}
+                  />
+                  {/* {errors.Status && (
+                    <p className={`${styles.errors_msg}`}>This field is required</p>
+                  )} */}
+                </div>
+
+                {/* <div className={`${styles.form_field} ${styles.fileBorder}`}>
+                  <input
+                    className={`${styles.fileInput}`}
+                    type="file"
+                    name="Resume"
+                    id="fileResume"
+                    placeholder="Upload resume *"
+                    accept=".pdf,.doc,.docx"
+                    {...register("Resume", {
+                      required: true,
+                      maxLength: 79,
+                    })}
+                  />
+                  
+                  {errors.Resume && (
+                    <p className={`${styles.errors_msg}`}>This field is required</p>
+                  )}
+                </div> */}
+                
+                <div className={`${styles.form_field}`}>
+                  <textarea
+                    className=""
+                    type="text"
+                    name="remarks"
+                    placeholder="Remarks, If Any "
+                    {...register("Remarks", {
+                      required: true,
+                      maxLength: 79,
+                    })}
+                  />
+                  {/* {errors.Remarks && (
+                    <p className={`${styles.errors_msg}`}>This field is required</p>
+                  )} */}
+                </div>
+                <div className={`${styles.btn_box}`}>
+                  <a href="#" rel="noreferrer">
+                    <span className="span_btn white_btn">
+                      <button className="btn_project_default">Submit</button>
+                    </span>
+                  </a>
+                </div>
+                {isSubmited && (
+                  <p className={`${styles.thank_you} para_sm mt_20`}>
+                    Thank you for your message. Someone from our team will get back
+                    shortly!
+                  </p>
+                )}
+              </form>
+            </div>
           </div>
         </div>
       </div>
